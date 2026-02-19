30e65f62510eb611fcd5896283726373041e4071
nicolasal97 <nicolasal97@curta.zedat.fu-berlin.de>
2026-02-19 09:10:39 +0100

    modules installed

--- preprocessing.bash
+++ preprocessing.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=run_preprocessing
-#SBATCH --output=../logs/preprocessing/slurm/run_preprocessing_%j.out
+#SBATCH --output=logs/preprocessing/slurm/run_preprocessing_%j.out
 
 #SBATCH --partition=scavenger
 #SBATCH --account=agfritz
@@ -11,8 +11,8 @@
 
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:h100:1
-#SBATCH --mem-per-cpu=10G
-#SBATCH --time=00:01:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --time=00:00:10
 
 echo "Loading modules..."
 module purge
@@ -22,7 +22,7 @@ module load virtualenv/20.32.0-GCCcore-14.3.0
 
 echo "Setting folders..."
 export HF_HOME=/scratch/nicolasal97/.cache/huggingface
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export PYTORCH_ALLOC_CONF=expandable_segments:True
 export TOKENIZERS_PARALLELISM=false
 export CUDA_LAUNCH_BLOCKING=1
 export TORCH_USE_CUDA_DSA=1
@@ -30,10 +30,10 @@ export NLTK_DATA=/home/nicolasal97/nltk_data
 export TORCHDYNAMO_CAPTURE_SCALAR_OUTPUTS=1
 
 echo "Activating virtual environment..."
-source ../venv/preprocessing/bin/activate
+source venv/preprocessing/bin/activate
 
 echo "Running python script..."
-python3 -u ../src/preprocessing/paragrapher.py
+python3 -u src/preprocessing/paragrapher.py
 
 deactivate
 module purge
